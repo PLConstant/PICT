42c6ef8655db4ddf532e82a7418150034bd9c349
Patrick Constant <PLConstant96@gmail.com>
2022-01-15 23:23:47 -0500

    added plan of approach and documentation

---
+++ components/PhotoCard.jsx
@@ -0,0 +1,14 @@
+
+// Create a function
+const PhotoCard = (props) => {
+  // takes props to get image source, image size options, photographer and link, other misc options that may help
+  return (
+    <>
+    {/* an outer container set to occupy 18% or so of the parent conainer's width */}
+    {/* it has an image that fills its container up to its side and top margins */}
+    {/* the div continues for a few pixels to display the author and a link */}
+    </>
+  )
+}
+
+export default PhotoCard
--- components/Viewer.jsx
+++ components/Viewer.jsx
@@ -1,4 +1,13 @@
+import useSWR from 'swr'
+
+// Cast the traditional fetching function to a variable.
+// this is syntactic sugar which later makes useSWR more readable.
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
 const Viewer = () => {
+
+  const { data, error } = useSWR("URI", fetcher);
+
   return (
     <div className='flex flex-wrap border-4 border-gray-200/50 h-3/4 w-screen bg-slate-600 justify-between'>
       
