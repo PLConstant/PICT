bd49b9c5531f8774e6852c65bbc09c3c117ab524
Patrick Constant <PLConstant96@gmail.com>
2022-01-17 18:29:34 -0500

    added conditionally rendered home button

--- components/PhotoCard.jsx
+++ components/PhotoCard.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 // Create a function
 const PhotoCard = ({ props }) => {
-  const { alt, photographer, photographer_url, avg_color } = props;
-  const pic = props.src.medium;
-  // console.log('props in photoCard', props);
+  const { alt, photographer, photographer_url, avg_color, src } = props
+  const pic = src.medium;
+
+
   return (
-    <>
       <div 
       className='flex flex-col rounded-t min-h-350 m-4 w-9/50 relative'
       style={{backgroundColor: avg_color}}
@@ -17,14 +17,13 @@ const PhotoCard = ({ props }) => {
           layout="fill"
           objectFit='contain'
         />
+        <div 
+        className='rounded-b h-10 w-9/50'
+        style={{backgroundColor: avg_color}}
+        >
+          <p>{'...hello?'}</p>
+        </div>
       </div>
-      {/* <div 
-      className='rounded-b h-10 w-9/50'
-      style={{backgroundColor: avg_color}}
-      >
-        <p>{'...hello?'}</p>
-      </div> */}
-     </>
   )
 }
 
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -29,17 +29,17 @@ const handleSubmit = () => {
 };
 
   return(
-    // a parent flex container set to arrange its children centered along a column
-    // occupying 3/4ths of its paternal width in order to spread the buttons a reasonable distance away from itself
     <div className='flex flex-col w-3/4 justify-center items-center'>
       <h1 className='text-xl text-center w-screen text-blue-900 font-bold self-center'>Welcome to REC&apos;t - An image search service but <i>better</i></h1>
-      {/* A simple text field set to be responsive but with a minimum width */}
-        <input 
-        className='my-6 py-5 px-2 h-8 w-1/3 min-w-200 rounded' 
-        value={query}
-        onChange={handleChange}
-        onKeyPress={handleKeypress}
-        />
+      <input 
+      className='my-6 py-5 px-2 h-8 w-1/3 min-w-200 rounded' 
+      value={query}
+      onChange={handleChange}
+      onKeyPress={handleKeypress}
+      />
+      {pageDetails.display === 'search' ?
+      <button onClick={() => updatePage({...pageDetails, display: 'curated', page: 1})}>{'home'}</button> :
+      null}
     </div>
   )
 }
