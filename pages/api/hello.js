91bc128e706c49230c17f5b5feac2e90359b87f0
Patrick Constant <PLConstant96@gmail.com>
2022-01-18 09:41:32 -0500

    updating commits

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// A default example for Next.js API files. This application has no need for it.
+// Useful to keep around should future developers see a need to integrate this feature of Next.js.
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
